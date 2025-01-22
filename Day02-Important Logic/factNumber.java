63eda23cd2b52eb3351b5b96b12d0c91ec5c1d6c
Rahul <rajrahul00945@gmail.com>
2025-01-22 14:07:04 +0530

    Basic logic of Day 02

---
+++ Day02-Important Logic/factNumber.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class factNumber {
+
+    public static int calCulateFactorial(int n) { 
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return n * calCulateFactorial(n - 1);
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        System.out.println(calCulateFactorial(number)); 
+    }
+}
